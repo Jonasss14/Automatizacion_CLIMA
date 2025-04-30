767363d356bb7b7b0e427e572fc95fc2d4da7f5b
Jonas <jonaslunameriles@gmail.com>
2025-04-30 12:29:09 -0300

    Eliminando Archivos

--- start.sh
+++
@@ -1,3 +0,0 @@
-pip install -r requirements.txt
-prefect cloud login --key $prefect cloud login -k pnu_F2AUoIZCHeizaWmuS9oQN0TQIRqCWV0vHkUy --workspace $prefect cloud workspace set --workspace "jonas-luna/default"
-prefect worker start --pool "clima" --work-queue "default"
